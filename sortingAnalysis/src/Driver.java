b08a9e8073c085a2fd6760d039e23e72c59718ef
amerk65584 <merka@uw.edu>
2018-10-14 11:35:19 -0700

    inital commit

---
+++ sortingAnalysis/src/Driver.java
@@ -0,0 +1,15 @@
+/**
+ * Main driver for our sorting algorithms.
+ * Only functionality is to call various algorithms.
+ * @author Alex Merk
+ * @author Nicole Kauer
+ */
+public class Driver {
+
+    /**
+     * Just main.
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ sortingAnalysis/src/Sort.java
@@ -0,0 +1,31 @@
+/**
+ * Main class containing our sorting algorithms.
+ * @author Alex Merk.
+ * @author Nicole Kauer.
+ */
+public class Sort {
+
+    /**
+     * First real world data set.
+     */
+    private Object realData1[];
+
+    /**
+     * Second real world data set.
+     */
+    private Object realData2[];
+
+    /**
+     * First distribution data set.
+     */
+    private Object syntheticData1[];
+
+    /**
+     * Second distribution data set.
+     */
+    private Object syntheticData2[];
+
+    public Sort() {
+        super();
+    }
+}
