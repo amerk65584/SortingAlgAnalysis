99529c63fd6f506bd73bb5d88c5a594f2e8018d5
amerk65584 <merka@uw.edu>
2018-10-14 11:56:29 -0700

    construct, experiment methods and notes

--- sortingAnalysis/src/Sort.java
+++ sortingAnalysis/src/Sort.java
@@ -25,7 +25,38 @@ public class Sort {
      */
     private Object syntheticData2[];
 
+    /**
+     * Holds the size of the data set for re-initialization.
+     */
+    private int real1Size;
+    private int real2Size;
+    private int synthetic1Size;
+    private int synthetic2Size;
+
+    /**
+     * Makes calls to construct and fill data sets with values and perform the experiments.
+     */
     public Sort() {
         super();
+        construct();
+        experiment();
+    }
+
+    /**
+     * Calls methods to construct the 4 data sets from scratch.
+     */
+    private void construct() {
+        realData1 = new Object[real1Size];
+        realData2 = new Object[real2Size];
+        syntheticData1 = new Object[synthetic1Size];
+        syntheticData2 = new Object[synthetic2Size];
+    }
+
+    /**
+     * Runs the experiments and tracks overall run-times and memory usage.
+     * SUBJECT TO CHANGE
+     */
+    private void experiment() {
+
     }
 }
