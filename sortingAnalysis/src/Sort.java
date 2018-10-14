844cf99891b99ac047bbc773263d413cbc875588
amerk65584 <merka@uw.edu>
2018-10-14 12:19:38 -0700

    just messing around now

--- sortingAnalysis/src/Sort.java
+++ sortingAnalysis/src/Sort.java
@@ -1,3 +1,5 @@
+import jdk.nashorn.api.tree.ForInLoopTree;
+
 /**
  * Main class containing our sorting algorithms.
  * @author Alex Merk.
@@ -6,24 +8,9 @@
 public class Sort {
 
     /**
-     * First real world data set.
+     * Array to hold all of the data sets.
      */
-    private Object realData1[];
-
-    /**
-     * Second real world data set.
-     */
-    private Object realData2[];
-
-    /**
-     * First distribution data set.
-     */
-    private Object syntheticData1[];
-
-    /**
-     * Second distribution data set.
-     */
-    private Object syntheticData2[];
+    private Object data[];
 
     /**
      * Holds the size of the data set for re-initialization.
@@ -46,10 +33,26 @@ public class Sort {
      * Calls methods to construct the 4 data sets from scratch.
      */
     private void construct() {
-        realData1 = new Object[real1Size];
-        realData2 = new Object[real2Size];
-        syntheticData1 = new Object[synthetic1Size];
-        syntheticData2 = new Object[synthetic2Size];
+        buildReal1();
+        buildReal2();
+        buildSynthetic1();
+        buildSynthetic2();
+    }
+
+    private void buildReal1() {
+        data = new Object[real1Size];
+    }
+
+    private void buildReal2() {
+        data = new Object[real2Size];
+    }
+
+    private void buildSynthetic1() {
+        data = new Object[synthetic1Size];
+    }
+
+    private void buildSynthetic2() {
+        data = new Object[synthetic2Size];
     }
 
     /**
@@ -57,6 +60,62 @@ public class Sort {
      * SUBJECT TO CHANGE
      */
     private void experiment() {
+
+    }
+
+    /**
+     * Experiment controller for the first real data set.
+     */
+    private void realData1Experiments() {
+
+    }
+
+    /**
+     * Experiment controller for the second real data set.
+     */
+    private void realData2Experiments() {
+
+    }
+
+    /**
+     * Experiment controller for the first synthetic data set.
+     */
+    private void synetheticData1Experiment() {
+
+    }
+
+    /**
+     * Experiment controller for the second synthetic data set.
+     */
+    private void syntheticData2Experiment() {
+
+    }
+
+    /**
+     * Analyzes the run time of the sorting algorithms based on the data size of the input.
+     */
+    private void runTime_dataSize() {
+
+    }
+
+    /**
+     * Analyzes the run time of the sorting algorithms based on the degree of sortedness of the input.
+     */
+    private void runTime_degSort() {
+
+    }
+
+    /**
+     * Analyzes the memory usage of the sorting algorithms based on the data size of the input.
+     */
+    private void memUse_dataSize() {
+
+    }
+
+    /**
+     * Analyzes the memory usage of the sorting algorithms based on the degree of sortedness of the input.
+     */
+    private void memUse_degSort() {
 
     }
 }
